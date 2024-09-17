33fd415e0e2e969da678bc470f4df6a1c8f63561
ZigRorer <zdrorer@gmail.com>
2024-09-16 20:15:40 -0400

    Studio updated

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -8,7 +8,7 @@ public class MenuItem {
     private boolean isNew;
     private boolean isOutOfStock;
 
-    public MenuItem(String name, double price, String category, boolean isNew, boolean isOutOfStock) {
+    public MenuItem(String name, double price, String category, String description, boolean isNew, boolean isOutOfStock) {
         this.name = name;
         this.price = price;
         this.category = category;
