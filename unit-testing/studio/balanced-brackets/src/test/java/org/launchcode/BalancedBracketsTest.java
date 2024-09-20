c3a3c07e03465444cde3957280a25aac4fc9619b
ZigRorer <zdrorer@gmail.com>
2024-09-19 21:06:32 -0400

    Studio completed

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,6 +7,54 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void launchcodeInBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+    @Test
+    public void codeInBrackets () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+    @Test
+    public void afterBrackets () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+    @Test
+    public void multipleBrackets () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode[]"));
+    }
+    @Test
+    public void singleBracket () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+    @Test
+    public void noBrackets () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+    @Test
+    public void singleBracketNoChar () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void singleOpenBracketNoChar () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void threeBrackets () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]["));
+    }
+    @Test
+    public void reversedBrackets () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void reversedWithChar () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]LaunchCode["));
+    }
+    @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
