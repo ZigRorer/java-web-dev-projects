8c4a2b9c97b151b47ad9faafd67334199c383c4b
ZigRorer <zdrorer@gmail.com>
2024-09-20 13:38:40 -0400

    Examples updated

---
+++ inheritance/testing-inheritance/exercises/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main extends HouseCat {
+    HouseCat garfield = new HouseCat("Garfield", 12);
+
+    public Main(String aName, double aWeight) {
+        super(aName, aWeight);
+    }
+    //garfield.eat();
+    //System.out.println(garfield.isTired());   // prints true
+
+    //HouseCat spike = new HouseCat("Spike");
+    //System.out.println(spike.getWeight()); // prints 13
+}
---
+++ inheritance/testing-inheritance/exercises/src/test/java/Tests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tests {
+    @Test
+    public void inheritsSuperInFirstConstructor() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat", 7);
+        assertEquals(7, keyboardCat.getWeight(), .001);
+    }
+}
