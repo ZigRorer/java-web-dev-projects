4577dd0cf81dc693360a1a7efee5fbab583de14c
ZigRorer <zdrorer@gmail.com>
2024-09-26 21:32:01 -0400

    Studio updated

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         // TODO: Declare and initialize a CD and a DVD object.
-
+        CD myCd = new CD("Greatest Hits", 700, "Music", "CD-R");
+        DVD myDvd = new DVD("Inception", 4700, "Movie", "DVD-R");
+        myCd.spinDisc();
+        myDvd.spinDisc();
+        myCd.reportInfo();
+        myDvd.reportInfo();
         // TODO: Call each CD and DVD method to verify that they work as expected.
     }
+}
 }
